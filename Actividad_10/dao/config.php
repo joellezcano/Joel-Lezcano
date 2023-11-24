7835ccc02844ced4640bb642485a8a65f9ced6f1
Joel-Lezcano <joellezcano52@gmail.com>
2023-11-24 15:38:11 -0300

    Actualización

---
+++ Actividad_10/dao/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('SERVER', 'localhost:3306');
+    define('USER', 'root');
+    define('PASSWORD', 'Pass.1234567');
+    define('DATABASE', 'base23');
+    define('CHARSET', 'utf-8');
+?>
